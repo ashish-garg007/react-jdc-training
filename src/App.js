e9bebc8e73eec1a3c9f3495c37262b92e6887685
ashish.garg <ashish.garg@appirio.com>
2017-08-24 16:15:07 +0530

    Latest code

--- src/App.js
+++ src/App.js
@@ -3,21 +3,81 @@ import logo from './logo.svg';
 import './styles/App.css';
 import Header from './components/Header';
 import Content from './components/Content';
+import logProps from './components/utilities/logProps';
+import PrivateRoute from './pages/PrivateRoute';
+import Home from './pages/Home';
+import About from './pages/About';
+import Protected from './pages/Protected';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header title="Welcome to React JDC Training" onHeaderClick={this.onHeaderClick} />
-        <Content text="Jaipur is awesome" />
-      </div>
-    );
-  }
+  state = {
+    title: 'Welcome to React JDC Traingin',
+    auth: {
+      isAuthenticated: false
+    }
+  };
+
+  login = (username, password) => {
+    //TODO: Your login logic here
+    this.setState({
+      auth: {
+        isAuthenticated: true
+      }
+    });
+  };
+
+  logout = () => {
+    // TODO: your logout logic
+    this.setState({
+      auth: {
+        isAuthenticated: false
+      }
+    });
+  };
+  setTitle = title => {
+    this.setState({ title });
+  };
 
   onHeaderClick = event => {
     event.preventDefault();
     window.alert('You pressed the header');
   };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header {...this.state} onHeaderClick={this.onHeaderClick} />
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
+            {this.state.auth.isAuthenticated &&
+              <Link to="/protected">Protected</Link>}
+          </nav>
+          <Route
+            exact
+            path="/"
+            render={props =>
+              <Home login={this.login} auth={this.state.auth} {...props} />}
+          />
+          <Route
+            path="/about"
+            render={props => <About setTitle={this.setTitle} {...props} />}
+          />
+          <PrivateRoute
+            path="/protected"
+            auth={this.state.auth}
+            render={props =>
+              <Protected
+                auth={this.state.auth}
+                logout={this.logout}
+                {...props}
+              />}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default logProps(App);
--- src/components/Content.test.js
+++ src/components/Content.test.js
@@ -4,16 +4,23 @@ import { shallow, mount } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import Content from './Content';
 
-describe('Content', () => {
+describe('<Content>', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Content />, div);
+    let setTitle = jest.fn();
+    ReactDOM.render(<Content setTitle={setTitle} />, div);
   });
 
   it('properly shows given text', () => {
-    const wrapper = shallow(
-        <Content text="test content component"/>
-      );
-      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    let setTitle = jest.fn();
+    const output = shallow(<Content text="Unit Tests" setTitle={setTitle} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should set the title of its parent', () => {
+    let setTitle = jest.fn();
+    const output = mount(<Content text="Unit Tests" setTitle={setTitle} />);
+    output.find('button').simulate('click');
+    expect(setTitle).toHaveBeenCalled();
   });
 });
